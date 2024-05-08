557c728826d8bff1910fc54781c85af163d5255f
Your Name <you@example.com>
2024-05-08 02:48:09 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -1,90 +1,96 @@
+#include "treemap.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "treemap.h"
 
 typedef struct TreeNode TreeNode;
 
-
 struct TreeNode {
-    Pair* pair;
-    TreeNode * left;
-    TreeNode * right;
-    TreeNode * parent;
+  Pair *pair;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode *parent;
 };
 
 struct TreeMap {
-    TreeNode * root;
-    TreeNode * current;
-    int (*lower_than) (void* key1, void* key2);
+  TreeNode *root;
+  TreeNode *current;
+  int (*lower_than)(void *key1, void *key2);
 };
 
-int is_equal(TreeMap* tree, void* key1, void* key2){
-    if(tree->lower_than(key1,key2)==0 &&  
-        tree->lower_than(key2,key1)==0) return 1;
-    else return 0;
+int is_equal(TreeMap *tree, void *key1, void *key2) {
+  if (tree->lower_than(key1, key2) == 0 && tree->lower_than(key2, key1) == 0)
+    return 1;
+  else
+    return 0;
 }
 
-
-TreeNode * createTreeNode(void* key, void * value) {
-    TreeNode * new = (TreeNode *)malloc(sizeof(TreeNode));
-    if (new == NULL) return NULL;
-    new->pair = (Pair *)malloc(sizeof(Pair));
-    new->pair->key = key;
-    new->pair->value = value;
-    new->parent = new->left = new->right = NULL;
-    return new;
-}
-
-TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
-    TreeMap * new = (TreeMap *)malloc(sizeof(TreeMap));
-    new->root = NULL;
-    new->current = NULL;
-    new->lower_than = lower_than;
-    return new;
-    //new->lower_than = lower_than;
-}
-
-
-void insertTreeMap(TreeMap * tree, void* key, void * value) {
-
-}
-
-TreeNode * minimum(TreeNode * x){
-
+TreeNode *createTreeNode(void *key, void *value) {
+  TreeNode *new = (TreeNode *)malloc(sizeof(TreeNode));
+  if (new == NULL)
     return NULL;
+  new->pair = (Pair *)malloc(sizeof(Pair));
+  new->pair->key = key;
+  new->pair->value = value;
+  new->parent = new->left = new->right = NULL;
+  return new;
 }
 
-
-void removeNode(TreeMap * tree, TreeNode* node) {
-
+TreeMap *createTreeMap(int (*lower_than)(void *key1, void *key2)) {
+  TreeMap *new = (TreeMap *)malloc(sizeof(TreeMap));
+  new->root = NULL;
+  new->current = NULL;
+  new->lower_than = lower_than;
+  return new;
+  // new->lower_than = lower_than;
 }
 
-void eraseTreeMap(TreeMap * tree, void* key){
-    if (tree == NULL || tree->root == NULL) return;
+void insertTreeMap(TreeMap *tree, void *key, void *value) {
+  if (tree == NULL || tree->root == NULL) {
+    tree->root = createTreeNode(key, value);
+    tree->current = tree->root;
+  } else {
+    TreeNode *aux = tree->root;
+    TreeNode *aux2 = NULL;
+    while (aux != NULL) {
+      if (tree->lower_than(key, aux->pair->key)) {
+        aux2 = aux;
+        aux = aux->left;
 
-    if (searchTreeMap(tree, key) == NULL) return;
-    TreeNode* node = tree->current;
-    removeNode(tree, node);
-
+      } else {
+        aux2 = aux;
+        aux = aux->right;
+      }
+    }
+    TreeNode *new = createTreeNode(key, value);
+    new->parent = aux2;
+    if (tree->lower_than(key, aux2->pair->key))
+      aux2->left = new;
+    else {
+      aux2->right = new;
+    }
+    tree->current = new;
+  }
 }
 
+TreeNode *minimum(TreeNode *x) { return NULL; }
 
+void removeNode(TreeMap *tree, TreeNode *node) {}
 
+void eraseTreeMap(TreeMap *tree, void *key) {
+  if (tree == NULL || tree->root == NULL)
+    return;
 
-Pair * searchTreeMap(TreeMap * tree, void* key) {
-    return NULL;
+  if (searchTreeMap(tree, key) == NULL)
+    return;
+  TreeNode *node = tree->current;
+  removeNode(tree, node);
 }
 
+Pair *searchTreeMap(TreeMap *tree, void *key) { return NULL; }
 
-Pair * upperBound(TreeMap * tree, void* key) {
-    return NULL;
-}
+Pair *upperBound(TreeMap *tree, void *key) { return NULL; }
 
-Pair * firstTreeMap(TreeMap * tree) {
-    return NULL;
-}
+Pair *firstTreeMap(TreeMap *tree) { return NULL; }
 
-Pair * nextTreeMap(TreeMap * tree) {
-    return NULL;
-}
+Pair *nextTreeMap(TreeMap *tree) { return NULL; }
