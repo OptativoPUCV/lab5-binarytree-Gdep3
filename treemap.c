6eb109e8dce4229187c399112f9f9224e06f2ae9
Your Name <you@example.com>
2024-05-09 01:04:07 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -104,6 +104,9 @@ Pair *searchTreeMap(TreeMap *tree, void *key) {
 
 Pair *upperBound(TreeMap *tree, void *key) { return NULL; }
 
-Pair *firstTreeMap(TreeMap *tree) { return NULL; }
+Pair *firstTreeMap(TreeMap *tree) { 
+  TreeNode *aux = tree->root;
+  return aux->pair; 
+}
 
 Pair *nextTreeMap(TreeMap *tree) { return NULL; }
