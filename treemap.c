8064938e549127fab01b8515d830019d58451051
Your Name <you@example.com>
2024-05-13 01:29:36 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -146,6 +146,9 @@ Pair *searchTreeMap(TreeMap *tree, void *key) {
 }
 
 Pair *upperBound(TreeMap *tree, void *key) {
+  if(tree == NULL || tree->root == NULL){
+    return NULL;
+  }
   TreeNode *aux = tree->root;
   TreeNode *ub = NULL;
   while (aux != NULL) {
@@ -164,7 +167,6 @@ Pair *upperBound(TreeMap *tree, void *key) {
       return NULL;
     }
   }
-
   return NULL;
 }
 
